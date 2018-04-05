5ed649124019b29f8790bc9156221677584b4fbf
EzeG <eze94ale@gmail.com>
2018-04-05 00:19:43 -0300

    Revert "cambio el settings"
    This reverts commit 16aede3b20cba6e77cd44c5c49334cfb166779b9.

--- noir/settings.py
+++ noir/settings.py
@@ -113,3 +113,5 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'emails')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'eze94ale@gmail.com'
+EMAIL_HOST_PASSWORD = '7@ezeale@7'
